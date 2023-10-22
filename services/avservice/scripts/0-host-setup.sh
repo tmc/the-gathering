f0b6294cec0afb85b3635c65f49b6141d9e6e640
Travis Cline <travis.cline@gmail.com>
2023-10-21 20:11:16 -0700

    add additional setup scripts

---
+++ services/avservice/scripts/0-host-setup.sh
@@ -0,0 +1,17 @@
+sudo apt-get install -y git
+sudo apt update
+curl -L -o chrome-remote-desktop_current_amd64.deb \
+    https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+
+sudo systemctl disable lightdm.service
+
+sudo apt install --assume-yes task-xfce-desktop
+
+curl -L -o google-chrome-stable_current_amd64.deb \
+https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb
